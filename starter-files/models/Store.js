9dc9f7d7252a5c3683a3e51b1107581e16ffb60c
windraner <c1net@rambler.ru>
2018-09-20 14:33:10 +0300

    23 finished

--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -3,45 +3,60 @@ mongoose.Promise = global.Promise;
 const slug = require('slugs');
 
 const storeSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: 'Please enter a store name!'
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
     },
-    slug: String,
-    description:  {
-        type: String,
-        trim: true
-    },
-    tags: [String],
-    created: {
-        type: Date,
-        default: Date.now
-    },
-    location: {
-        type: {
-            type: String,
-            default: 'Point'
-        },
-        coordinates: [{
-            type: Number,
-            required: 'You must supplay coordinates'
-        }],
-        address: {
-            type: String,
-            required: 'You must supplay address'
-        }
-    },
-    photo: String
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coordinates!'
+    }],
+    address: {
+      type: String,
+      required: 'You must supply an address!'
+    }
+  },
+  photo: String
 });
 
-storeSchema.pre('save', function(next) {
-    if(!this.isModified('name')) {
-        next(); // skip it
-        return; // stop this function
-    }
-    this.slug = slug(this.name);
-    next();
+storeSchema.pre('save', async function(next) {
+  if (!this.isModified('name')) {
+    next(); // skip it
+    return; // stop this function from running
+  }
+  this.slug = slug(this.name);
+  // find other stores that have a slug of wes, wes-1, wes-2
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
+  next();
+  // TODO make more resiliant so slugs are unique
 });
+
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ]);
+}
 
 module.exports = mongoose.model('Store', storeSchema);
